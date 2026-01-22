335e7a9b03aa97cc1ee5260d1dd54ba0b0b92360
pratikhobro <pratikdevkota200@gmail.com>
2026-01-22 15:04:30 +0545

    MLProject

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import pandas as pd
 import sys
-
 import dill
 import pickle
 from sklearn.metrics import r2_score
@@ -21,7 +20,7 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models,params):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
         for i in range(len(list(models))):
@@ -32,14 +31,15 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params):
             gs.fit(X_train,y_train)
             #model training using best params
             model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
             #prediction
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
             #r2 score
             train_model_score=r2_score(y_train,y_train_pred)
-            test_model_score=r2_score(y_test,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
 
-            report[list(model.keys())[i]]=test_model_score
+            report[list(models.keys())[i]]=test_model_score
             return report
         
     except Exception as e:
@@ -51,6 +51,3 @@ def load_object(file_path):
             return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
-    
-    
-    
