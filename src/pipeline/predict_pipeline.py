5d937bf5246704a5dc5dbdab31732e082e49f7b3
pratikhobro <pratikdevkota200@gmail.com>
2026-01-23 15:47:44 +0545

    Docker

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -4,7 +4,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
 
-class predictpipeline:
+class PredictPipeline:
     def __init__(self):
         pass
     def predict(self,features):
